dc532774687cb2518f451d42e473710e45a66b7b
root.seyo <root.seyo@gmail.com>
2018-01-03 13:17:49 +0900

    Update index.py

--- index.py
+++ index.py
@@ -19,7 +19,7 @@ def handler(event, context):
     send_message_to_slack(messages=msg)
                  
                         
-def send_message_to_slack(url="<Replace Slack incomming url", messages=None):
+def send_message_to_slack(url="<Replace Slack incomming url>", messages=None):
     '''
     Send message to slack
 
